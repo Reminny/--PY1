7eb8a6918917d778f524e226edd45cfdb4520900
Reminny <115098052+Reminny@users.noreply.github.com>
2022-10-19 20:20:09 +0300

    Delete Лабораторная работа 3 directory

--- Лабораторная работа 3/task1.py
+++
@@ -1,9 +0,0 @@
-src = not False and True or False and not True
-
-# result = True and True or False and False # избавляемся от отрицаний
-# result = True or False # избавляемся от логической "И"
-# result = True # избавляемся от логического "ИЛИ"
-
-result = True  # TODO подставить результат выражения
-
-print(src == result)
